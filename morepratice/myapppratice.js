8524adcff5fe33b1392e90527ec1f4a2e8121d9b
Taylor <bigtaw7@gmail.com>
2019-08-01 14:28:44 -0400

    improved

--- morepratice/myapppratice.js
+++ morepratice/myapppratice.js
@@ -5,26 +5,38 @@ window.onload=function(){
    // var go = document.querySelector("#con");
    // var button = document.querySelector("#sub");
    mysubmit.addEventListener("click", function(){
-   const games = {
-       Overwatch: "25.00",
-       Halo: "50.00",
-       Madden: "30.00",
-       GearsofWar: "40.00",
-       Fortnight: "50.00"
-       };
+        const games = {
+        Overwatch: "25.00",
+        Halo: "50.00",
+        Madden: "30.00",
+        GearsofWar: "40.00",
+        Fortnight: "50.00"
+        };
        var mygame = xbox.value;
-       var mygame = mygame.split(" , ");
-       // console.log(mygame[0]);
+        // var mygame = mygame.split(" , ");
+        // console.log(mygame[0]);
+        var onList= false;
        for(var gametitle in games) {
+           /*
            mygame = games[gametitle];
                console.log(games);
-               for(var i = 0 )
+               for(var i = 0 );
+               */
+              console.log(gametitle);
+              if (mygame === gametitle){
+                checkgame();
+                onList= true;
+              }
+
        }
-       for (var i = 0; i < mygame.length; i++){
-           var games = mygames
+       if(onList === false){
+         notgame();  
        }
-   }
-)};
+    //    for (var i = 0; i < mygame.length; i++){
+    //        var games = mygames
+    //    }
+    }
+    )};
         function checkgame(){
             alert("This game is checked off the list");
         }
