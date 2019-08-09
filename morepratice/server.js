c942c9cf3d8fc5c400c19029a41047d35651cf76
Taylor <bigtaw7@gmail.com>
2019-08-09 08:54:13 -0400

    new

--- morepratice/server.js
+++ morepratice/server.js
@@ -1,20 +1,20 @@
 
-var http = require('http');
-var url = require('url');
+// var http = require('http');
+// var url = require('url');
 
-var server  = http.createServer(function(request, response){
-    response.setHeader('Access-Control-Allow-Origin', '*')
+// var server  = http.createServer(function(request, response){
+//     response.setHeader('Access-Control-Allow-Origin', '*')
 
-     var parseUrl = url.parse(request.url, true);
-     var filePath = '.' + parseUrl.pathname;
-     console.log(filePath);
+//      var parseUrl = url.parse(request.url, true);
+//      var filePath = '.' + parseUrl.pathname;
+//      console.log(filePath);
      
 
      
-     response.writeHead(200,{'Content-Type': 'text/html'});
-     response.write("I got all the games");
-     response.end("I have completed my wishlist!");
- });
+//      response.writeHead(200,{'Content-Type': 'text/html'});
+//      response.write("I got all the games");
+//      response.end("I have completed my wishlist!");
+//  });
 
- server.listen(8080, function(){ console.log('server is listening');
-});
+//  server.listen(8080, function(){ console.log('server is listening');
+// });
